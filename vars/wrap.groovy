2428fb6432efa0fab06b6bd246d47cf016b041c5
Yasassri Ratnayake <ycrnet@gmail.com>
2022-06-26 13:33:54 -0400

    Add content

--- vars/wrap.groovy
+++ vars/wrap.groovy
@@ -1,8 +1,15 @@
 // vars/buildPlugin.groovy
 def call(body) {
     // evaluate the body block, and collect configuration into the object
+    println("Before body")
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
-    println(config.name)
+    body()
+    println("After body")
+
+    // now build, based on the configuration provided
+    node {
+        echo "${config.name}"
+    }
 }
