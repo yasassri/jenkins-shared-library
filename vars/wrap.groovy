2bb4b9d660e492d36b26a863e2435470241ab09e
Yasassri Ratnayake <ycrnet@gmail.com>
2022-06-26 13:15:46 -0400

    Add content

--- vars/wrap.groovy
+++ vars/wrap.groovy
@@ -1,6 +1,6 @@
 def call(Closure body) {
         echo "in the wrapper"
-        println(body.println())
+        println(body.getMaximumNumberOfParameters())
         //println(body.getProperty('imageName'))
         //body()
 }
